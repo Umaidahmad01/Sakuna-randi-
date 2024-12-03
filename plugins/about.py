8d97b92400f5099c0b7647e225eee57afcf26f11
rohit3607 <rohitplayer87089@gmail.com>
2024-12-03 17:33:08 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -79,7 +79,7 @@ async def start_command(client: Client, message: Message):
         return
     else:
         reply_markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
+            [InlineKeyboardButton("CLOSE", callback_data='close'),
              InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')]
         ])
         await message.reply_photo(
