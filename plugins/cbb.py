5381b5260e2878b4f5a496e54a7d47cbe1c07ec1
Umaid <ahmedumaid707@gmail.com>
2025-03-04 12:17:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,16 +1,15 @@
-from pyrogram import Client 
-from pyrogram import __version__
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from bot import Bot
-from config import *
+from config import OWNER_ID, ABOUT_TXT, HELP_TXT, START_MSG
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from database.database import add_user, del_user, full_userbase, present_user
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
+    if data == "help":
         await query.message.edit_text(
-            text=("<b>» ᴏᴡɴᴇʀ: <a href=http://t.me/Kakashi_The_Star>𝚜ᴏʙᴜᴢ</a>\n» 𝙵ᴏᴜɴᴅᴇʀ : <a href=http://t.me/Shimla_ki_Mirch>xᴀ-ʟɪɴ</a>\n» ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href=https://t.me/backupanimepoint>ᴠᴏʀᴛᴇ𝚡 𝙽ᴇᴛᴡᴏʀᴋ</a>\n» ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/+oM4CJ2tdJ3o3Y2Jl>ᴀɴɪᴍᴇ ᴠᴏʀᴛᴇ𝚡 </a>\n» ᴏɴɢᴏɪɴɢ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/RyumaOngoingAnime>ᴏɴɢᴏɪɴɢ ᴠᴏʀᴛᴇ𝚡 </a>\n» ᴠᴏʀᴛᴇ𝚡 𝙽ᴇᴡ𝚜 : <a href=https://t.me/+In6rvaacWcA4NDc1>ᴠᴏʀᴛᴇ𝚡 𝙽ᴇᴡ𝚜</a>\n» ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/i_killed_my_clan>ᴏʙɪᴛᴏ</a></b>"),
+            text=HELP_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -21,10 +20,32 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton('• ʜᴏᴍᴇ', callback_data='start'),
+                     InlineKeyboardButton('ᴄʟᴏꜱᴇ •', callback_data='close')]
+                ]
+            )
+        )
+    elif data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("• ғᴏʀ ᴍᴏʀᴇ •", url='https://t.me/anime_sub_society')],
+                    [InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data='about'),
+                     InlineKeyboardButton("ʜᴇʟᴘ •", url='https://t.me/ahss_help_zone')],
+                    [InlineKeyboardButton("• ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ •", url='https://t.me/society_network')],
+                ])
+        )
+    
     elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
         except:
-            pass
-    
+            pass      
